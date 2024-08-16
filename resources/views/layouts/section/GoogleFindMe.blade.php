560d732ed6e086e89553756cdc48b947243432f2
nakorns <nakorns@ssi-steel.com>
2024-08-16 09:41:26 +0700

    fix img

--- resources/views/layouts/section/GoogleFindMe.blade.php
+++ resources/views/layouts/section/GoogleFindMe.blade.php
@@ -2,9 +2,9 @@
   <div class="container aos-init aos-animate" data-aos="fade-up"> 
       <div class="text-center">
           <h3><i class="bi bi-geo-alt-fill"></i>ร้านเหล็กใกล้ฉัน</h3>
-          <iframe width="600" height="300" src="http://maps.google.de/maps?hl=th&q=ร้านเหล็กใกล้ฉัน&ie=UTF8&t=&z=17&iwloc=B&output=embed"
+          <iframe width="600" height="300" src="https://maps.google.de/maps?hl=th&q=ร้านเหล็กใกล้ฉัน&ie=UTF8&t=&z=17&iwloc=B&output=embed"
            frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-              <a href="http://www.siteway.de/maps-generator/" title="ร้านเหล็กใกล้ฉัน">(c) Homepage Google Maps Generator</a></iframe>
+              <a href="https://www.siteway.de/maps-generator/" title="ร้านเหล็กใกล้ฉัน">(c) Homepage Google Maps Generator</a></iframe>
       </div>
        </div> 
  </section>
