6402a91889969d479465606cf2755463c4acf096
nakorns <nakorns@ssi-steel.com>
2024-08-26 18:16:34 +0700

    ui profile

--- resources/views/manage/MyProfile.blade.php
+++ resources/views/manage/MyProfile.blade.php
@@ -2,17 +2,15 @@
     <div class="container   aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
         <div class="d-flex align-items-center py-2">
             <div class="flex-grow-1">
-                <a href="/manage/profile">
-                    <div>
-                         
-                    </div> 
-                    <span class="float-end text-success  ">
+                <a href="/manage/profile"> 
+                    <span class="text-center text-success  ">
                         <p>
                             <img src="{{ session('account')['profile_display_url'] }}" class="  rounded  " alt="โปรไฟล์"
                                 loading="lazy" style="width: 5rem;minmax(3rem, 48px);" />
                         </p>
-                        <span class="float-start text-success text-bold">{{ session('account')['display_name'] }} </span>
-                    </br>
+                        <p>
+                            <span class="text-center text-success text-bold">{{ session('account')['display_name'] }} </span>
+                        </p> 
                         <p>
                         <i class="bi bi-pencil-square" style=" "></i>
                         แก้ไขข้อมูลส่วนตัว    
