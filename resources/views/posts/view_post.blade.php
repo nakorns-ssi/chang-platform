37b693d0a7b6d31eabd8041ca7f60374a0f2bd10
nakorns <nakorns@ssi-steel.com>
2024-08-29 16:54:59 +0700

    btn share

--- resources/views/posts/view_post.blade.php
+++ resources/views/posts/view_post.blade.php
@@ -11,6 +11,9 @@
             use App\helper\util; 
             $Product_image_url = []; 
             $link = '/';
+            $btn_share_title =   mb_substr($model->posts_content,0,100)  ;
+            $btn_share_text =   mb_substr($model->posts_content,0,100)  ;
+            $btn_share_url =   url()->current();
             ?>
             
             <div class="container pb-5">  
@@ -21,15 +24,30 @@
                                 @include('posts.view_user_profile' ,['account' => $account ,'posts'=>$model])
                                 
                             </div>
-                            <div class="col-sm-4">
-                                 <!-- AddToAny BEGIN --> 
-                                 <div class="a2a_kit a2a_kit_size_32 a2a_default_style py-2 py-2 d-flex justify-content-end"> 
-                                    <a class="a2a_button_facebook"></a> 
-                                    <a class="a2a_button_line"></a>
-                                    <a class="a2a_button_copy_link"></a>
-                                    </div>
-                                    <script async src="https://static.addtoany.com/menu/page.js"></script>
-                                    <!-- AddToAny END -->  
+                            <div class="col-sm-4  text-end">
+                                <button type="button btn-sm" id="btn_share" class="btn btn-success"><i class="bi bi-share"></i> แชร์ให้เพื่อน</button>
+                                <input type="hidden" id="btn_share_title" value="{{$btn_share_title}}" > 
+                                <input type="hidden" id="btn_share_text" value="{{$btn_share_text}}" > 
+                                <input type="hidden" id="btn_share_url" value="{{$btn_share_url}}" > 
+                                <script>
+                                    const shareData = {
+                                    title: "MDN",
+                                    text: "Learn web development on MDN!",
+                                    url: "https://developer.mozilla.org",
+                                    };
+
+                                    const btn = document.querySelector("#btn_share"); 
+
+                                    // Share must be triggered by "user activation"
+                                    btn.addEventListener("click", async () => {
+                                    try {
+                                        await navigator.share(shareData);
+                                        resultPara.textContent = "MDN shared successfully";
+                                    } catch (err) {
+                                        resultPara.textContent = `Error: ${err}`;
+                                    }
+                                    });
+                                </script>
                             </div>
                         </div> 
                         <hr class="w-100" />
