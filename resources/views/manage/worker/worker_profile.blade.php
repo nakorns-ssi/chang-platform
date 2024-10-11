b37320f9d3ea2d2c9f9f08ab1dfe241d08bfc3ed
nakorns <nakorns@ssi-steel.com>
2024-10-11 12:57:52 +0700

    add log viewer

--- resources/views/manage/worker/worker_profile.blade.php
+++ resources/views/manage/worker/worker_profile.blade.php
@@ -12,258 +12,82 @@
            
         </div>
     </header><!-- End Header -->
-
-    <section class="container  " style="margin-top:25px;">
-
-        <?php
-        $actionPath = '/manage/worker/worker_profile_save';
-        ?>
  
-        <div class="row justify-content-center">
-            <div class="col-md-8  ">
-                <div class="card text-center mb-5 pt-4">
-                    <span class="  text-danger">
-                        <h4>โปรไฟล์ช่าง</h4> 
-                        <span>กรอกข้อมูลที่ถูกต้องเพื่อความสะดวกในการติดต่อ</span>
-                    </span>
-                    
-                    <form action="{{ $actionPath }}" method="post" name="frm_n" id="frm_n" class="  ">
-                        @csrf
-                        <div class="card-body">
-                            <?php 
-                                if(!isset($model['profile_work_history'][0])){
-                                    $model['profile_work_history'][0]=null;
-                                }
-                                if(!isset($model['profile_project'][0])){
-                                    $model['profile_project'][0]=null;
-                                }
-
-                                if(!isset($model['work_category'][0])){
-                                    $model['work_category'][0]=null;
-                                }
-                                if(!isset($model['work_category_other'][0])){
-                                    $model['work_category_other'][0]=null;
-                                }
-
-                                if(!isset($model['work_sub_category'][0])){
-                                    $model['work_sub_category'][0]=null;
-                                }
-                                if(!isset($model['work_sub_category_other'][0])){
-                                    $model['work_sub_category_other'][0]=null;
-                                }
-                                if(!isset($model['skill'][0])){
-                                    $model['skill'][0]=null;
-                                }
-                                if(!isset($model['skill_other'][0])){
-                                    $model['skill_other'][0]=null;
-                                }
-                                if(!isset($model['product'][0])){
-                                    $model['product'][0]=null;
-                                }
-                                if(!isset($model['product_other'][0])){
-                                    $model['product_other'][0]=null;
-                                }
-                                ?>
-                            <div class="     px-1 text-start  my-3 "> 
-                                <div class="row justify-content-center align-items-center">
-                                    <label for="profile_work_history" class="h5">ประวัติการทำงาน</label>
-                                    <textarea class="form-control"   name="model[profile_work_history][]" 
-                                    rows="3">{{$model['profile_work_history'][0]}}</textarea>
+    <section class="  ">
+        <div class="container "> 
+            <div class="row  bg-white border rounded-4">
+                <div class="col-12 border-bottom sticky-top  ">
+                    <div class=" mb-0     shadow">  
+                            <div class="row g-0 mx-2 my-3 row-cols-3">
+                                <div class="col-4 ">
+                                    <img src="{{ session('account')['profile_display_url'] }}" class="  img-fluid rounded-circle mb-2  "
+                                    width="64" height="64" alt="โปรไฟล์" loading="lazy" /> 
                                 </div>
-                            </div>
-                            <div class="     px-1 text-start  my-3 "> 
-                                <div class="row justify-content-center align-items-center">
-                                    <label for="profile_project" class="h5">ผลงาน</label>
-                                    <textarea class="form-control"   name="model[profile_project][]" 
-                                    rows="3">{{$model['profile_project'][0]}}</textarea>
+                                <div class="col-4 px-2 ">
+                                  <span class="text-dark text-truncate"> {{ session('account')['profile_display_name'] }}</span> 
+                                    <br />
+                                  <span class="text-dark  ">{{ session('account')['display_name'] }}</span>   
                                 </div>
+                                <div class="col-4 row align-items-center px-0"> 
+                                   <span  ><i class="bi bi-chevron-right"></i></span> 
+                               </div>
+                            </div>  
+                    </div>
+                </div> 
+                <div class="col-sm-6 col-xl-4  "> 
+                    <div class="  row  my-2 ">
+                        <div class="col-4">
+                            <div class="col-4  ">
+                                <div class="  ">
+                                    <div class="fw-bold mx-1"> 00 </div> 
+                                </div> 
+                                <p><small>คะแนน</small></p>
                             </div>
-                             
-                            <div class="container text-center mt-2"> 
-                                <h5>ประเภทงาน</h5>
-                                <?php 
-                                $work_category = [ 
-                                     "งานเหล็ก" ,"งานไฟฟ้า","งานประปา","งานสี","งานปูน", "งานไม้"  
-                                ]; 
-                                    ?>
-                                    <div class="row g-2">
-                                        @foreach ($work_category as $key => $item)
-                                        <?php 
-                                        $checked = '';
-                                        if(isset($model['work_category'])){
-                                            if( in_array($item ,$model['work_category']) ){
-                                                $checked = 'checked';
-                                            }
-                                        }
-                                        ?>
-                                        <div class=" col-4 col-sm-3 flex-fill ">
-                                            <label class="w-100" for="work_category{{$key}}"> 
-                                                <div class="card bg-primary-subtle fs-6  mb-1 p-2">
-                                                    {{$item}}
-                                                  <input type="checkbox" id="work_category{{$key}}" 
-                                                  name="model[work_category][]" value="{{$item}}"
-                                                  {{$checked}}
-                                                  >
-                                                </div>
-                                            </label>
-                                        </div> 
-                                        @endforeach 
-                                    </div> 
-                                    <div class="row g-2">
-                                        <div class="mb-3  text-start">
-                                            <label for="work_category_other" class="form-label">ประเภทงาน อื่นๆ</label>
-                                            <input type="text" class="form-control" id="work_category_other"
-                                            name="model[work_category_other][]" value="{{$model['work_category_other'][0]}}" 
-                                             placeholder=" ระบุ">
-                                          </div>
-                                    </div>
-                            </div>
-
-                            <div class="container text-center mt-2"> 
-                                <h5>ประเภทงานย่อย</h5>
-                                <?php 
-                                $work_sub_category = [ 
-                                    "งานบำรุงรักษา","งานซ่อมแซม","งานติดตั้ง","งานผลิต" 
-                                ]; 
-                                    ?>
-                                    <div class="row g-2">
-                                        @foreach ($work_sub_category as $key => $item)
-                                        <?php 
-                                        $checked = '';
-                                        if(isset($model['work_sub_category'])){
-                                            if( in_array($item ,$model['work_sub_category']) ){
-                                                $checked = 'checked';
-                                            }
-                                        }
-                                        ?>
-                                        <div class=" col-4 col-sm-3 flex-fill ">
-                                            <label class="w-100" for="work_sub_category{{$key}}"> 
-                                                <div class="card bg-primary-subtle fs-6  mb-1 p-2">
-                                                    {{$item}}
-                                                  <input type="checkbox" id="work_sub_category{{$key}}" 
-                                                  name="model[work_sub_category][]" value="{{$item}}"
-                                                  {{$checked}}
-                                                  >
-                                                </div>
-                                            </label>
-                                        </div> 
-                                        @endforeach 
-                                    </div> 
-                                    <div class="row g-2">
-                                        <div class="mb-3 text-start ">
-                                            <label for="work_sub_category_other" class="form-label">ประเภทงานย่อย อื่นๆ</label>
-                                            <input type="text" class="form-control" id="work_sub_category_other"
-                                            name="model[work_sub_category_other][]" value="{{$model['work_sub_category_other'][0]}}" 
-                                             placeholder=" ระบุ">
-                                          </div>
-                                    </div>
-                            </div>
-
-                            <div class="container text-center mt-2"> 
-                                <h5>ทักษะ</h5>
-                                <?php 
-                                $skill = [ 
-                                    "เชื่อมฟลักซ์คอร์","เชื่อมแก๊ส","เชื่อมทิก" ,"เชื่อมแม๊ก","เชื่อมอาร์กโลหะด้วยมือ"
-                                    ,"ประกอบโครงสร้างเหล็ก" ,"ตัดเหล็กกล้าคาร์บอนด้วยแก๊ส" ,"กลึง"
-                                ]; 
-                                    ?>
-                                    <div class="row g-2">
-                                        @foreach ($skill as $key => $item)
-                                        <?php 
-                                        $checked = '';
-                                        if(isset($model['skill'])){
-                                            if( in_array($item ,$model['skill']) ){
-                                                $checked = 'checked';
-                                            }
-                                        }
-                                        ?>
-                                        <div class=" col-4 col-sm-3 flex-fill ">
-                                            <label class="w-100" for="skill{{$key}}"> 
-                                                <div class="card bg-primary-subtle fs-6  mb-1 p-2">
-                                                    {{$item}}
-                                                  <input type="checkbox" id="skill{{$key}}" 
-                                                  name="model[skill][]" value="{{$item}}"
-                                                  {{$checked}}
-                                                  >
-                                                </div>
-                                            </label>
-                                        </div> 
-                                        @endforeach 
-                                    </div> 
-                                    <div class="row g-2">
-                                        <div class="mb-3 text-start ">
-                                            <label for="skill_other" class="form-label">ทักษะ อื่นๆ</label>
-                                            <input type="text" class="form-control" id="skill_other"
-                                            name="model[skill_other][]" value="{{$model['skill_other'][0]}}" 
-                                             placeholder=" ระบุ">
-                                          </div>
-                                    </div>
-                            </div>
-
-                            <div class="container text-center mt-2"> 
-                                <h5>สินค้า</h5>
-                                <?php 
-                                $product = [ 
-                                    "เหล็กดัด","มุ้งลวด","ประตู" ,"รั้ว","โรงรถ","บ้านนอคดาวน์" 
-                                ]; 
-                                    ?>
-                                    <div class="row g-2">
-                                        @foreach ($product as $key => $item)
-                                        <?php 
-                                        $checked = '';
-                                        if(isset($model['product'])){
-                                            if( in_array($item ,$model['product']) ){
-                                                $checked = 'checked';
-                                            }
-                                        }
-                                        ?>
-                                        <div class=" col-4 col-sm-3 flex-fill ">
-                                            <label class="w-100" for="product{{$key}}"> 
-                                                <div class="card bg-primary-subtle fs-6  mb-1 p-2">
-                                                    {{$item}}
-                                                  <input type="checkbox" id="product{{$key}}" 
-                                                  name="model[product][]" value="{{$item}}"
-                                                  {{$checked}}
-                                                  >
-                                                </div>
-                                            </label>
-                                        </div> 
-                                        @endforeach 
-                                    </div> 
-                                    <div class="row g-2">
-                                        <div class="mb-3 text-start ">
-                                            <label for="product_other" class="form-label">สินค้า อื่นๆ</label>
-                                            <input type="text" class="form-control" id="product_other"
-                                            name="model[product_other][]" value="{{$model['product_other'][0]}}" 
-                                             placeholder=" ระบุ">
-                                          </div>
-                                    </div>
-                            </div>
-
-                           
-                             
-
-                           
-
-                             
-                            <div class="buttons px-4 mt-4">
-                                <button class="btn btn-primary text-white btn-block rating-submit" onclick="do_save()"
-                                    type="button">บันทึก</button>
-
-                            </div>
+                            <div class="col-4  ">
+                                <div class="  ">
+                                    <div class="fw-bold mx-1"> 00 </div> 
+                                </div> 
+                                <p><small>คะแนน</small></p>
+                            </div> 
+                            <div class="col-4  ">
+                                <div class="  ">
+                                    <div class="fw-bold mx-1"> 00 </div> 
+                                </div> 
+                                <p><small>คะแนน</small></p>
+                            </div> 
                         </div>
-                    </form>
-                </div>
+                        <div class="col-8">
+                             
+                        </div>
+                       
+                    </div> 
+                </div> 
+                <div class="col-sm-6 col-xl-8">
+ 
+                </div> 
+                <div class="col-12 border-top"> 
+                    <div class="row g-0 mx-2 my-3 row-cols-3">
+                        <div class="col-4 ">
+                            <img src="{{ session('account')['profile_display_url'] }}" class="  img-fluid rounded-circle mb-2  "
+                            width="64" height="64" alt="โปรไฟล์" loading="lazy" /> 
+                        </div>
+                        <div class="col-4 px-2 ">
+                            <span class="text-dark text-truncate"> {{ session('account')['profile_display_name'] }}</span> 
+                            <br />
+                            <span class="text-dark  ">{{ session('account')['display_name'] }}</span>   
+                        </div>
+                        <div class="col-4 row align-items-center px-0"> 
+                            <span  ><i class="bi bi-chevron-right"></i></span> 
+                        </div>
+                    </div>   
+                </div> 
             </div>
         </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-8  "></div>
         </div>
-
-
-
     </section>
+
+
     <?php if (Session::has('alert')):
       $alert = Session::get('alert');
       ?>
